8ebe26d5c01f132dc1d1af289ae0f3064cd9c27f
ThariniV <thariniv422@live.com>
2016-06-22 22:29:18 +0530

    Create oddrange.c

---
+++ oddrange.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+	long long int range1,range2,i;
+	scanf("%lld%lld",&range1,&range2);
+	for(i=range1;i<=range2;i++)
+	{
+		if(i%2!=0)
+			printf("%lld ",i);
+	}
+	return 0;
+}
