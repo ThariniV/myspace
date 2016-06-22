45d38087f78e02df85e752afe033042f28f7f25b
ThariniV <thariniv422@live.com>
2016-06-22 23:20:53 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+	int number;
+	scanf("%d",&number);
+	printf("%d",fact(number));
+	return 0;
+}
+int fact(int n)
+{
+	if(n>=1)
+		return n*fact(n-1);
+	else
+		return;
+}
